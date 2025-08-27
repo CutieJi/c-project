810a800dd3eaa3c6dc28b2344733e6e3b6a47d50
Rosh <roshjingel@gmail.com>
2025-08-28 00:45:56 +0800

    UPDATED

--- group5_activity.c
+++ group5_activity.c
@@ -6,7 +6,7 @@
 int main()
 {
     int input = 7;
-    int i, j, s;
+    int i, j, s, u;
 
     printf("\n1. Line Input %d:\n\n", input); // CONTRIBUTOR: INGEL
     for(i = 0; i < input; i++)
@@ -16,18 +16,26 @@ int main()
     printf("\n\n");
 
     printf("\n2. StrippedLine Input %d:\n\n", input); // CONTRIBUTOR: SUMAGAYSAY
-    for (i = 0; i < input; i++) {
-        if (i % 2 == 0) {
+    for(i = 0; i < input; i++)
+    {
+        if(i % 2 == 0)
+        {
             printf("*");
-        } else {
+        }
+        else
+        {
             printf("_");
         }
     }
     printf("\n\n");
 
     printf("\n3. Square Input %d:\n\n", input); // CONTRIBUTOR: SUNGA
-    for (int i = 0; i < input; i++) {
-        for (int j = 0; j < input; j++) printf("*");
+    for(i = 0; i < input; i++)
+    {
+        for(j = 0; j < input; j++)
+        {
+            printf("*");
+        }
         printf("\n");
     }
     printf("\n\n");
@@ -52,11 +60,14 @@ int main()
     printf("\n\n");
 
     printf("5. Triangle Input %d:\n\n", input); // CONTRIBUTOR: SUMAGAYSAY
-    for (int i = 0; i <= input; i++) {
-        for (int star = 0; star < input - i; star++) {
+    for(i = 0; i <= input; i++)
+    {
+        for(s = 0; s < input - i; s++)
+        {
             printf("*");
         }
-        for (int under = 0; under < i; under++) {
+        for(u = 0; u < i; u++)
+        {
             printf("_");
         }
         printf("\n");
@@ -64,11 +75,14 @@ int main()
     printf("\n\n");
 
     printf("\n6. ReverseTriangle Input %d:\n\n", input); // CONTRIBUTOR: SUNGA
-    for (int i = 0; i < input; i++) {
-        for (int star = 0; star <= i; star++) {
+    for(i = 0; i < input; i++)
+    {
+        for(s = 0; s <= i; s++)
+        {
             printf("*");
         }
-        for (int under = 0; under < input - i - 1; under++) {
+        for(u = 0; u < input - i - 1; u++)
+        {
             printf("_");
         }
         printf("\n");
@@ -95,14 +109,18 @@ int main()
     printf("\n\n");
 
     printf("\n8. ReverseIsocelesTriangle Input %d:\n\n", input); // CONTRIBUTOR: SUMAGAYSAY
-    for (int i = 0; i <= input / 2; i++) {
-        for (int lunder = 0; lunder < i; lunder++) {
+    for(i = 0; i <= input / 2; i++)
+    {
+        for(int lunder = 0; lunder < i; lunder++)
+        {
             printf("_");
         }
-        for (int stars = 0; stars < input - 2*i; stars++) {
+        for(s = 0; s < input - 2*i; s++)
+        {
             printf("*");
         }
-        for (int runder = 0; runder < i; runder++) {
+        for(int runder = 0; runder < i; runder++)
+        {
             printf("_");
         }
         printf("\n");
@@ -110,14 +128,18 @@ int main()
     printf("\n\n");
 
     printf("\n9. Hourglass Input %d:\n\n", input); // CONTRIBUTOR: SUNGA
-    for (int i = 0; i <= input / 2; i++) {
-        for (int under = 0; under < i; under++) {
+    for(i = 0; i <= input / 2; i++)
+    {
+        for(u = 0; u < i; u++)
+        {
             printf("_");
         }
-        for (int star = 0; star < input - 2 * i; star++) {
+        for(s = 0; s < input - 2 * i; s++)
+        {
             printf("*");
         }
-        for (int under = 0; under < i; under++) {
+        for(u = 0; u < i; u++)
+        {
             printf("_");
         }
         printf("\n");
@@ -160,11 +182,16 @@ int main()
     printf("\n\n");
 
     printf("\n11. Zero Input %d:\n\n", input); // CONTRIBUTOR: SUMAGAYSAY
-      for (int row = 0; row < input; row++) {       
-        for (int col = 0; col < input; col++) {  
-            if (row == 0 || row == input-1 || col == 0 || col == input-1) {
+      for(i = 0; i < input; i++)
+      {       
+        for(j = 0; j < input; j++)
+        {  
+            if(i == 0 || i == input-1 || j == 0 || j == input-1)
+            {
                 printf("*");   
-            } else {
+            }
+            else
+            {
                 printf("_");   
             }
         }
@@ -173,16 +200,33 @@ int main()
     printf("\n\n");
 
     printf("\n12. ArrowUp Input %d:\n\n", input); // CONTRIBUTOR: SUNGA
-    for (int i = 1; i <= input; i += 2) {
-        for (int s = 0; s < (input - i) / 2; s++) printf("_");
-        for (int j = 0; j < i; j++) printf("*");
-        for (int s = 0; s < (input - i) / 2; s++) printf("_");
+    for(i = 1; i <= input; i += 2)
+    {
+        for(s = 0; s < (input - i) / 2; s++)
+        {
+            printf("_");
+        }
+        for(j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        for(s = 0; s < (input - i) / 2; s++)
+        {
+            printf("_");
+        }
         printf("\n");
     }
-    for (int i = 0; i < input / 2; i++) {
-        for (int s = 0; s < (input - 3) / 2; s++) printf("_");
+    for(i = 0; i < input / 2; i++)
+    {
+        for(s = 0; s < (input - 3) / 2; s++)
+        {
+            printf("_");
+        }
         printf("***");
-        for (int s = 0; s < (input - 3) / 2; s++) printf("_");
+        for(s = 0; s < (input - 3) / 2; s++)
+        {
+            printf("_");
+        }
         printf("\n");
     }
     printf("\n\n");
@@ -221,11 +265,16 @@ int main()
     printf("\n\n");
 
     printf("\n14. X Input %d:\n\n", input); // CONTRIBUTOR: SUMAGAYSAY
-    for (int i = 0; i < input; i++) {       
-        for (int zstar = 0; zstar < input; zstar++) {   
-            if (zstar == i || zstar == input - i - 1) {
+    for(i = 0; i < input; i++)
+    {       
+        for(s = 0; s < input; s++)
+        {   
+            if(s == i || s == input - i - 1)
+            {
                 printf("*");   
-            } else {
+            }
+            else
+            {
                 printf("_");   
             }
         }
@@ -234,11 +283,21 @@ int main()
     printf("\n\n");
 
     printf("\n15. BowTie Input %d:\n\n", input); // CONTRIBUTOR: SUNGA
-    for (int i = 0; i < input; i++) {
+    for(i = 0; i < input; i++)
+    {
         int row = (i <= input/2) ? i : input - i - 1;
-        for (int j = 0; j <= row; j++) printf("*");
-        for (int j = 0; j < input - 2*(row+1) + 1; j++) printf("_");
-        for (int j = 0; j <= row && input - 2*(row+1) + 1 >= 0; j++) printf("*");
+        for(j = 0; j <= row; j++)
+        {
+            printf("*");
+        }
+        for(j = 0; j < input - 2*(row+1) + 1; j++)
+        {
+            printf("_");
+        }
+        for(j = 0; j <= row && input - 2*(row+1) + 1 >= 0; j++)
+        {
+            printf("*");
+        }
     printf("\n");
 }
     printf("\n\n");
