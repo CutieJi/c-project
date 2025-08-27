c6b3b2272805b794981fba3d7c0686f2458679f5
Rosh <roshjingel@gmail.com>
2025-08-27 11:26:02 +0800

    UPDATED

--- 1g5.c
+++ 1g5.c
@@ -33,6 +33,28 @@ void parallelogram(int value)
     }
 }
 
+void isocelesTriangle(int value)
+{
+    int i, j;
+
+    for(i = 0; i < value; i++)
+    {
+        for(j = 0; j < value - 1 - i; j++)
+        {
+            printf("_");
+        }
+        for(j = 0; j < (2 * i + 1); j++)
+        {
+            printf("*");
+        }
+        for(j = 0; j < value - 1 - i; j++)
+        {
+            printf("_");
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
     int value1 = 5, value2 = 7;
@@ -45,6 +67,11 @@ int main()
 
     printf("\nDrawParallelogram Input 5:\n"); parallelogram(value1);
     printf("DrawParallelogram Input 7:\n"); parallelogram(value2);
+
+    printf("\n");
+
+    printf("\nDrawIsocelesTriangle Input 5:\n"); isocelesTriangle(value1);
+    printf("DrawIsocelesTriangle Input 7:\n"); isocelesTriangle(value2);
 
     return 0;
 }
